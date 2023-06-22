2e7a44f281b906c0a491ed5c01eb3da58e8900c5
RojinTehrani <roji.tehrani@gmail.com>
2023-06-23 01:44:37 +0330

    Buy charge completed

--- app/src/main/java/com/example/sharj_app/BuychargeActivity.java
+++ app/src/main/java/com/example/sharj_app/BuychargeActivity.java
@@ -3,32 +3,47 @@ package com.example.sharj_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BuychargeActivity extends AppCompatActivity {
-    private Button menu,BuyCharge;
+     Button menu,BuyCharge;
     int Amount=2000;
-    int Operator=0;
+    int Operator=2;
     CircleImageView mci,irancell,rightel;
     Button five,ten,twenty;
+    EditText PhoneNumber;
+    String phonenumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_buycharge);
         menu=findViewById(R.id.menu);
         mci=findViewById(R.id.mci);
@@ -38,6 +53,7 @@ public class BuychargeActivity extends AppCompatActivity {
         ten=findViewById(R.id.ten);
         twenty=findViewById(R.id.twenty);
         BuyCharge=findViewById(R.id.BuyCharge);
+        PhoneNumber = findViewById(R.id.PhoneNumber);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,14 +66,6 @@ public class BuychargeActivity extends AppCompatActivity {
 
             }
         });
-
-        Retrofit retrofit=new Retrofit
-                .Builder()
-                .baseUrl("https://topup.pec.ir/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        Users users=retrofit.create(Users.class);
-
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,20 +88,73 @@ public class BuychargeActivity extends AppCompatActivity {
 
             }
         });
+        mci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Operator=1;
+            }
+        });
+        irancell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Operator=2;
+            }
+        });
+        rightel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Operator=3;
+            }
+        });
+
+
         BuyCharge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mci.isPressed()){
-                    Operator=0;
+                phonenumber = PhoneNumber.getText().toString();
+
+                String url = "http://spid3r.ir/?charge&ph=" + phonenumber + "&op=" + Operator + "&am=" + Amount;
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {//put a log in here///
+                            Log.d("respon ====", response + "");
+                            String url = response.getString("url");
+                            Uri webpage = Uri.parse(url);
+                            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+
+                            if (intent.resolveActivity(getPackageManager()) != null) {
+                                startActivity(intent);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.d("err ===", error.getMessage() + ""); //run kon test bgir
+
+                    }
+                });
+
+                RequestQueue rozhi = Volley.newRequestQueue(BuychargeActivity.this);
+                rozhi.add(request);
+
+                if (PhoneNumber.getText().toString().length()==0) {
+                    Toast.makeText(BuychargeActivity.this, "لطفا شماره تلفن خود را وارد نمایید", Toast.LENGTH_SHORT).show();
                 }
-                if(irancell.isPressed()){
-                    Operator=1;
-                }
-                if(rightel.isPressed()){
-                    Operator=2;
-                }
+
             }
         });
+
+
+
+
+
     }
 
 
