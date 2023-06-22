0f2b1ed4812812a23aa60f4001fc6c2c71ee2979
RojinTehrani <roji.tehrani@gmail.com>
2023-06-22 13:46:04 +0330

    Design MainActivity

--- app/src/main/java/com/example/sharj_app/MainActivity.java
+++ app/src/main/java/com/example/sharj_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sharj_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,24 +9,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button menu;
+//    private CardView menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        menu=findViewById(R.id.menu);
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMainActivity();
-            }
-            public void openMainActivity(){
-                Intent intent;
-                intent = new Intent(MainActivity.this,CheckbillActivity.class);
-                startActivity(intent);
-
-            }
-        });
     }
 }
